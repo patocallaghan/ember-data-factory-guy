c599b4b0dd1b5614c3d19f02d0f3b06f3331bea7
Pat O'Callaghan <pat@intercom.io>
2020-05-06 21:38:09 +0100

    Upgrade to Ember 3.16 LTS

--- testem.js
+++ testem.js
@@ -1,24 +1,28 @@
-/* eslint-env node */
+'use strict';
+
 module.exports = {
-  framework: 'qunit',
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   reporter: 'dot',
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_start_timeout: 120,
   browser_args: {
     Chrome: {
-      mode: 'ci',
-      args: [
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
         '--headless',
-        '--disable-gpu',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
-        '--window-size=1440,900',
-        '--no-sandbox',
-      ],
-    },
-  },
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
 };
